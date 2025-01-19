a8ff8c394e09b20f39073dab01fa163696ee0c03
pedroandrade932 <86068661+pedroandrade932@users.noreply.github.com>
2025-01-18 22:00:47 -0300

    Pagina de pesquisa bruta

--- script_search.js
+++ script_search.js
@@ -1,21 +1,26 @@
-let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b42cefeaf183405d95940739d2eaaca6';
-mostrarNoticias = document.getElementById('noticias');
+function search(){
+    let pesquisa = window.document.querySelector("#search").value
+    let url = `https://newsapi.org/v2/everything?q=${pesquisa}&language=pt&pageSize=25&sortBy=publishedAt&apiKey=b42cefeaf183405d95940739d2eaaca6`
+    let mostrarNoticias = window.document.getElementById('noticias')
+    mostrarNoticias.innerText = ""
 
-fetch(url).then(resp =>{
-    return resp.json();
-}).then(dados=>{
-    console.log(dados);
-    let noticias = (dados.articles);
-        noticias.map(function(numero){
-            let div = document.createElement('div');
-            div.className = "news";
-            div.innerHTML = `
-                            <img class='imagem' src=${numero.urlToImage}>
-                            <h2> ${numero.title}</h2>
-                            <p> ${numero.description}</p>
-                            <a href =${numero.url} target='blank'>
-                            <p class="link"> Leia mais </p></a>`;
-            mostrarNoticias.appendChild(div);
-                    
-        })
-})
+    fetch(url).then(resp =>{
+        return resp.json()
+    }).then(dados=>{
+        console.log(dados)
+        let noticias = (dados.articles)
+            noticias.map(function(numero){
+                let div = document.createElement('div')
+                div.className = "news"
+                if (numero.title != "[Removed]"){
+                    div.innerHTML = `
+                                    <img class='imagem' src=${numero.urlToImage}>
+                                    <h2> ${numero.title}</h2>
+                                    <p> ${numero.description}</p>
+                                    <a href =${numero.url} target='blank'>
+                                    <p class="link"> Leia mais </p></a>`
+                    mostrarNoticias.appendChild(div)
+                }
+            })
+    })
+}
